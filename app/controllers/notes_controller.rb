634b947161dda4216a89525a7977e153dffff8bd
Birzhan Serik <birzhans14@gmail.com>
2022-12-03 04:37:14 +0600

    Update notes_controller.rb

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,54 @@
 class NotesController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @notes = current_user.notes
+  end
+
+  def show
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def edit
+  end
+
+  def create
+    @note = current_user.notes.new(note_params)
+
+    if @note.save
+      render turbo_stream: [
+        turbo_stream.prepend('notes', @note),
+        turbo_stream.replace('new-note-form', partial: 'form', locals: { note: Note.new })
+      ]
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @note.destroy
+    render turbo_stream: turbo_stream.remove(@note)
+  end
+
+  private
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:body, :date)
+  end
 end
