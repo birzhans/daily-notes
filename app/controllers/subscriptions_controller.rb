0ac83010ed2be1ae4ff62690bfef69defa28f097
Birzhan Serik <birzhans14@gmail.com>
2022-12-05 05:34:47 +0600

    remove subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,4 +5,11 @@ class SubscriptionsController < ApplicationController
     @followings = current_user.followings
     @notes = Note.where(user: @followings).ordered.first(10)
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    @subscription = Subscription.find_by(user: @user, subscriber: current_user)
+    @subscription.destroy
+    render turbo_stream: turbo_stream.remove(@user)
+  end
 end
